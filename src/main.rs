69eca2f2cb449491764b5ee90efe9130f03f5df8
Giuseppe Daniele Consoli <giuseppe@ktz.one>
2021-12-12 10:09:01 +0100

    support pathfinding

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use bracket_lib::prelude::*;
-mod map;
-mod map_builder;
 mod camera;
 mod components;
+mod map;
+mod map_builder;
 mod spawner;
 mod systems;
 mod turn_state;
-// TODO Implementing Combat
+// TODO Implementing Comba@installed t
 // simplify module access creating prelude
 mod prelude {
     pub use bracket_lib::prelude::*;
-    pub use legion::*;
-    pub use legion::world::SubWorld;
     pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
-    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH/2;
-    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT/2;
-    pub use crate::map::*;
-    pub use crate::map_builder::*;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
     pub use crate::components::*;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
     pub use crate::turn_state::*;
@@ -38,7 +38,6 @@ struct State {
 }
 
 impl State {
-    
     fn new() -> Self {
         // NOTE: Initialize world and resources
         let mut ecs = World::default();
@@ -48,7 +47,8 @@ impl State {
         spawn_player(&mut ecs, map_builder.player_start);
         // NOTE: Go through rooms, skip the first, transform each room entry to a Point using
         // .center(), run a closure on each location with for_each and call spawn_monster on each point
-        map_builder.rooms
+        map_builder
+            .rooms
             .iter()
             .skip(1)
             .map(|r| r.center())
@@ -63,9 +63,8 @@ impl State {
             resources,
             input_systems: build_input_scheduler(),
             player_systems: build_player_scheduler(),
-            monster_systems: build_monster_scheduler()
+            monster_systems: build_monster_scheduler(),
         }
-
     }
 }
 
@@ -86,16 +85,16 @@ impl GameState for State {
         // We're using a copy to get around the borrow checker
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
         match current_state {
-            TurnState::AwaitingInput => self.input_systems.execute(
-                &mut self.ecs,
-                &mut self.resources
-            ),
+            TurnState::AwaitingInput => self
+                .input_systems
+                .execute(&mut self.ecs, &mut self.resources),
             TurnState::PlayerTurn => {
-                self.player_systems.execute(&mut self.ecs, &mut self.resources);
+                self.player_systems
+                    .execute(&mut self.ecs, &mut self.resources);
             }
-            TurnState::MonsterTurn => {
-                self.monster_systems.execute(&mut self.ecs, &mut self.resources)
-            }
+            TurnState::MonsterTurn => self
+                .monster_systems
+                .execute(&mut self.ecs, &mut self.resources),
         }
         render_draw_buffer(ctx).expect("Render error");
     }
@@ -109,11 +108,10 @@ fn main() -> BError {
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
-        .with_font("terminal8x8.jpg", 8,8)
+        .with_font("terminal8x8.jpg", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, 
-            "dungeonfont.png")
-        .with_simple_console_no_bg(SCREEN_WIDTH*2, SCREEN_HEIGHT*2, "terminal8x8.jpg")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.jpg")
         .build()?;
 
     main_loop(context, State::new())
